bbd8f3a92564c1d35643b1b96c3b1c855ffff956
Antonio Rafael Ortega <AntRafa@MacBook-Pro-de-Antonio.local>
2018-03-03 10:06:19 -0300

    Mostrando primeira mensagem

--- Voffice/javawebpratico2018/cinema/src/main/java/br/com/voffice/jwp/cinema/controllers/HelloController.java
+++ Voffice/javawebpratico2018/cinema/src/main/java/br/com/voffice/jwp/cinema/controllers/HelloController.java
@@ -15,6 +15,7 @@ public class HelloController extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.getWriter().write("Hello World!!");
 	}
 	
 }
