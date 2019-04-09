1077b41584161c0d09ba059b2808388bf3470ab5
Antonio Rafael <antrafa@MacBook-Pro-de-Antonio.local>
2019-04-09 11:18:26 -0300

    Finalizando projeto

--- JavaScript/ReactJS/sistema-comentarios/src/firebase.js
+++ JavaScript/ReactJS/sistema-comentarios/src/firebase.js
@@ -4,12 +4,7 @@ import 'firebase/auth'
 
 // Initialize Firebase
 var config = {
-      apiKey: "AIzaSyDOd-wpVytRibQle7uSy8KPn7afqtWyEOw",
-      authDomain: "comments-antrafa.firebaseapp.com",
-      databaseURL: "https://comments-antrafa.firebaseio.com",
-      projectId: "comments-antrafa",
-      storageBucket: "comments-antrafa.appspot.com",
-      messagingSenderId: "907673694296"
+      
 };
 firebase.initializeApp(config);
 export const database = firebase.database()
